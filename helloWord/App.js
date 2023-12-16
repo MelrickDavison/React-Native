83217b658cdd5aea835831e6458151df600b4632
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-12-16 15:03:40 -0300

    Update App.js

--- helloWord/App.js
+++ helloWord/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
