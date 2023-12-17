77d3c22482d7f34f76cec5e04e62410f22febae9
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-12-17 10:16:24 -0300

    update

---
+++ AboutMe/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, StyleSheet, SafeAreaView, Text, Image, StatusBar } from 'react-native';
+
+export default function App() {
+let img = "./img/perfil.jpg";
+  return  (
+  <SafeAreaView style={styles.container}>
+  <StatusBar/>
+  <Image
+    source={{uri: img}}
+    style={styles.foto}
+  />
+  
+  <Image
+    source={require('./src/Images/droneOrla.jpg')}
+    style={styles.imgSmall}
+  />
+
+</SafeAreaView>
+);
+}
+
+  const styles = StyleSheet.create({
+    container: {
+    backgroundColor: '#000',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+    },
+    foto:{
+      height: 300,
+      width: '90%',
+      resizeMode: 'contain'
+    },
+    imgSmall:{
+      width: '90%',
+      height: 300,
+      resizeMode: 'cover'
+    }
+  });
