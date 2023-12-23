74699e53d5320394e7b1532f0a8e128776d06f07
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-12-23 17:14:39 -0300

    Update App.js

--- AboutMe/App.js
+++ AboutMe/App.js
@@ -1,41 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet, SafeAreaView, Text, Image } from 'react-native';
-import { useFonts } from 'react-native-google-fonts/GoogleFonts';
+import { View, StyleSheet, SafeAreaView, Text, Image, ScrollView} from 'react-native';
 
 export default function App() {
 
-  //let img = './img/perfil.jpg';
-  const [fontsLoaded] = useFonts({
-    Roboto: require('path-to-your-fonts/Roboto-Regular.ttf'),
-    'Roboto-Bold': require('path-to-your-fonts/Roboto-Bold.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return null; // Ou algum componente de carregamento enquanto as fontes estão sendo carregadas
-  }
-
  
 return (
-   
-
+  
    <SafeAreaView style={styles.container}>
-   
-      <View style={styles.header}>
+    <ScrollView> 
+    <View style={styles.header}>
         About Me
       </View>
       <Image
         source={require('./img/perfil.jpg')}
         style={styles.imgSmall}
       />
-
+      <Text style={styles.titulo}>
+        Melrick Davison
+      </Text>
+      <Text style={styles.text}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </Text>
+      </ScrollView>
+     
    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    background: 'linear-gradient(to bottom right, #10378a, #0b2978)',
-  flex: 1,
+    backgroundColor: '#10378a',
+    flex: 1,
   alignItems: 'center',
   justifyContent: 'center'
   },
@@ -52,10 +47,12 @@ const styles = StyleSheet.create({
     shadowOpacity: 3.6,
     shadowRadius: 4,
     elevation: 2,
+    position: 'fixed',
+    top: 150,
+    marginLeft: 22.6,
     },
 
   header:{
-    fontFamily: 'Roboto',
     color: '#fff',
     backgroundColor: '#000c55',
     shadowColor: 'black',
@@ -71,7 +68,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     fontSize: "50px"
-    
+  },
+  titulo: {
+    marginLeft: 55,
+    width: 300,
+    fontSize: '34px',
+    padding: '12px',
+    fontWeight: 'bold',
+    color: '#fff',
+    backgroundColor: '#051a67',
+    textAlign: 'center',
+    marginTop: 470,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+    elevation: 2, // Necessário para sombras aparecerem no Android
+  },
+
+  text:{
+    margin: 12,
+    paddingHorizontal: 15,
+    textAlign: 'justify',
+    fontSize: '24px',
+    color: '#fff',
   }
 });
 
