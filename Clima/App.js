c2bef60749c5b295b7f42156d04eb60ccac8686d
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2024-01-28 04:57:43 -0300

    update

--- Clima/App.js
+++ Clima/App.js
@@ -1,30 +1,59 @@
+import React, {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { View, SafeAreaView, Text, StyleSheet, StatusBar, TextInput, TouchableOpacity } from 'react-native';
-
+import { View, SafeAreaView,Image, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import axios from 'axios';
+    
 export default function App() {
+  const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const getWeather = async () => {
+  try {
+    // API utilizada: https://openweathermap.org/
+    const apiKey = 'b1a6c4e7d8151aff17c8882ad520c79c';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+    const response = await axios.get(apiUrl);
+    setWeatherData(response.data);
+  } catch (error) {
+    alert("Digite um local válido");
+    console.error('Erro ao obter dados do clima:', error);
+  }
+  }
   return (
     <SafeAreaView style={styles.container}>
     <StatusBar/>
     <Text style={styles.titulo}> ClimAtual </Text>
     
-    <Text> Digite um local </Text>
+    <Text style={styles.legenda}> Digite um local: </Text>
+
+    <View style={styles.Busca}>
     <TextInput
-    placeholder='Ex: Maceió'
+        style={styles.input}
+        onChangeText={text => setLocation(text)}
+        value={location}
+      />
+       <TouchableOpacity  onPress={getWeather}>
+       <Image
+        source={require('./src/image/pesquisa.png')}
+        style={styles.imgBusca}
     />
+      </TouchableOpacity>
+      </View>
     <View>
-
-      <TouchableOpacity>
-        <Text> Buscar </Text>
-      </TouchableOpacity>
-    
-      <TouchableOpacity>
-        <Text> Limpar </Text>
-      </TouchableOpacity>
+  
     </View>
 
-    <View style={styles.respostas}>
+    {weatherData && (
+        <View style={styles.respostasView}>
+          <Text style={styles.respostas}>Condição: {weatherData.weather[0].description}</Text>
+          <Text style={styles.respostas}>Temperatura: {(weatherData.main.temp).toFixed(1)} °C</Text>
+          <Text style={styles.respostas}>Umidade: {weatherData.main.humidity}%</Text>
+         
+        </View>
+      )}
 
-    </View>
+
 
    </SafeAreaView>
   );
@@ -33,11 +62,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0390fc',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   respostas: {
     textAlign: 'center',
+    color: 'white',
+    fontSize: 20,
+    padding: 10,
+  },
+  respostasView: {
+    backgroundColor: '#0339fc',
+    flexDirection: 'column',
+    borderRadius: "60",
+    borderStyle: 'solid',
+    borderWidth: '2px', 
+    borderColor: 'black',
+    shadowColor: 'black',
+    shadowOffset: { width: 3, height: 4 },
+    shadowOpacity: 3.6,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  Busca: {
+    flexDirection: 'row',
+    padding: '10px'
+  },
+  imgBusca: {
+    width: 30,
+    height: 30,
+    marginLeft: '10px',
+    top:  '3px',
+  
+  },
+  titulo: {
+    color: '#fff',
+    fontFamily:  'Arial',
+    fontWeight: 'bold',
+    fontSize: 48,
+    margin: '20px'
+  },
+
+  legenda:{
+    color:'#000',
+    fontSize: 15
+  },
+
+  input: {
+    height: 35, 
+    width: 200,
+    borderColor: '#000', 
+    color: '#fff',
+    borderWidth: 1, 
+    marginBottom: 10 
   }
-});
+})
